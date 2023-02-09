408f2f81cc158d3480b63f368d8b6350fac45a6e
samanyougarg <samanyugarg@gmail.com>
2023-02-09 20:17:18 +0530

    add google analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,25 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 import PlausibleProvider from 'next-plausible'
+import Script from 'next/script'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <PlausibleProvider domain="bhagavadgita.ai" customDomain="https://analytics.bhagavadgita.io" selfHosted>
-      <Component {...pageProps} />
-    </PlausibleProvider>
+    <>
+      <Script strategy="lazyOnload" src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`} />
+      <Script strategy="lazyOnload" id="gtag">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+            page_path: window.location.pathname,
+          });
+        `}
+      </Script>
+      <PlausibleProvider domain="bhagavadgita.ai" customDomain="https://analytics.bhagavadgita.io" trackLocalhost selfHosted trackOutboundLinks taggedEvents>
+        <Component {...pageProps} />
+      </PlausibleProvider>
+    </>
   )
 }
