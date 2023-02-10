d0778406ca64ec28d5a5e25e16f03ebd60fadb4f
kartikp36 <kartikp2002@gmail.com>
2023-02-10 22:27:32 +0530

    feat: render about us page from notion

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,6 +3,10 @@ import type { AppProps } from 'next/app'
 import PlausibleProvider from 'next-plausible'
 import Script from 'next/script'
 
+import 'react-notion-x/src/styles.css'
+import 'prismjs/themes/prism-tomorrow.css'
+import 'katex/dist/katex.min.css'
+
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
