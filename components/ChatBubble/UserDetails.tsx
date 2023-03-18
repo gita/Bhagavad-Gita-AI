f32a11f5025638ed8f9cbfe30a520f1b0f6fd31a
Samit <Samit.prsl@gmail.com>
2023-03-18 20:40:27 +0530

    Tried To Fix Image Spacing

--- components/ChatBubble/UserDetails.tsx
+++ components/ChatBubble/UserDetails.tsx
@@ -15,7 +15,7 @@ const UserDetails = ({ sent }: UserDetailsProps) => {
         alt="Krishna"
         height={58}
         width={58}
-        className="flex-1 aspect-square h-10 w-10 md:h-14 md:w-14 rounded-full"
+        className="flex aspect-square h-10 w-10 md:h-14 md:w-14 rounded-full"
         src={sent ? "/Avatars/arjuna.png" : "/Avatars/krishna.png"}
       />
       <div
